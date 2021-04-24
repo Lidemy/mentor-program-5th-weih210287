a0dd8221e9a856052d3cfc7e059a2083debb4efd
gz <weih210287@gmial.com>
2021-04-25 01:21:11 +0800

    hw1&hw5 changed

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,11 +1,8 @@
 // hw1：印出星星
 // 給定 n（1<=n<=30），依照規律「印出」正確圖形
 function printStars(n) {
-	while(1 <= n && n <= 30) {
-		for(i = 1; i <= n; i++) {
-			console.log('*')
-		}
-		break
+	for(i = 1; i <= n; i++) {
+		console.log('*')
 	}
 }
 
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -10,12 +10,13 @@ function repeat(str, times) {
 	for(i = 1; i <= times; i++) {
 		strNew += str
 	}
-	return strNew;
+	return strNew
 }
 
 console.log(join([1, 2, 3], ''))  //正確回傳值：123
 console.log(join(["a", "b", "c"], "!"))  //正確回傳值：a!b!c
 console.log(join(["a", 1, "b", 2, "c", 3], ','))  //正確回傳值：a,1,b,2,c,3
+console.log(join(["aaa", "bb", "c", "dddd"], ',,') ) //正確回傳值：aaa,,bb,,c,,dddd
 
 console.log(repeat('a', 5))  //正確回傳值：aaaaa
 console.log(repeat('yoyo', 2))  //正確回傳值：yoyoyoyo
